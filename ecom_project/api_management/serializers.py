ab970aa6ae2193a6c3a00dd72aea2c30c3b84af7
Juniër <102233050+DhuJUICE@users.noreply.github.com>
2025-04-01 13:28:08 +0200

    Revert "setup initial django project for api gateway"
    This reverts commit 0cf7ab076d4932afe007fc3fdf91c5bfcf160f90.

--- ecom_project/api_management/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from member_management.models import MEMBER
-
-
-class MemberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MEMBER
-        fields = '__all__'
-
