ebb1599648297039a746c0b57bcfe029e0e46210
DhuJUICE <junierherandien84@gmail.com>
2025-05-15 04:26:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors'); //need to npm install cors
 //include the route files to be used
 const authRoutes = require('./routes/auth');
 const atmRoutes = require('./routes/atm');
+//const bankAccountRoutes = require('./routes/bank_account_mgmt');
 
 require('dotenv').config(); //need to npm install dotenv
 
@@ -14,5 +15,6 @@ app.use(express.json());
 //Define the actual url Routes to my routes files
 app.use('/auth', authRoutes);
 app.use('/atm', atmRoutes);
+//app.use('/bank-account', bankAccountRoutes);
 
 app.listen(3000, () => console.log('API Gateway running on port 3000'));
