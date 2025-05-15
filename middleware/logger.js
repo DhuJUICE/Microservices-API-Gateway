ff1fe6967243c3f9e1147f0386d614c75ca316d5
DhuJUICE <junierherandien84@gmail.com>
2025-05-15 04:30:44 +0200

    added logging file and installed winston

---
+++ middleware/logger.js
@@ -0,0 +1,21 @@
+// logger.js
+const { createLogger, format, transports } = require('winston'); //need to npm install winston
+const { combine, timestamp, printf, colorize } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
+  ),
+  transports: [
+    new transports.Console({ format: combine(colorize(), logFormat) }),
+    new transports.File({ filename: 'logs/app.log' })
+  ],
+});
+
+module.exports = logger;
